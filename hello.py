795ed8feb922936c612d4febf3af1e96eeb780ec
elenaiprou <elenaiprou@gmail.com>
2021-04-27 00:35:49 +0200

    pantalla pygame

---
+++ hello.py
@@ -0,0 +1,21 @@
+import pygame as pg
+
+pg.init() # Inicializar todos los controles de pygame
+pantalla = pg.display.set_mode((600,400))
+pg.display.set_caption("Hola")
+
+game_over = False
+
+while not game_over:
+    # Gestion de eventos
+    for event in pg.event.get():
+        pass
+
+
+    # Gestion del estado
+    print("Hola mundo")
+    
+    #Refrescar pantalla 
+    pantalla.fill((0,255,0))
+    pg.display.flip()
+
